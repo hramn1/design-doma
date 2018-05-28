b38f01a7e0279a6748505c61a1cd5deb187e0a62
Unknown <stmihailov@yandex.ru>
2018-05-28 21:33:57 +0300

    правки

--- js/curs.js
+++ js/curs.js
@@ -159,54 +159,53 @@ let heightLink = 0;
 let btnStart = document.querySelector('.btn--start');
 let btnOffer = document.querySelector('.btn--offer');
 
-// tarifiLink.forEach(function(item) {
-// item.addEventListener('click',function(evt) {
-//  let heightTop = 0;
-//   if(item.hash === '#tarifiId'){
-//     evt.preventDefault();
-//    heightLink = tarifiId.offsetTop;
-// } else if (item.hash === '#otzivi'){
-//       evt.preventDefault();
-//    heightLink = otziviId.offsetTop;
-// } else if (item.hash === '#grad'){
-//         evt.preventDefault();
-//    heightLink = gridId.offsetTop;
-// }
-//   function topset(){
-//     if(heightTop > heightLink -50){
-//       clearInterval(intervalOff)
-//     }
-//     heightTop+=50;
-//     window.scrollTo(0,heightTop);
-//   }
-//   let intervalOff = setInterval(topset, 13);
-//   })
-// });
-//
-// btnStart.addEventListener('click', function(evt) {
-//   let heightTop = window.pageYOffset;
-//   evt.preventDefault();
-//   heightLink = gridId.offsetTop;
-//   function topset(){
-//     if(heightTop > heightLink -50){
-//       clearInterval(intervalOff)
-//     }
-//     heightTop+=40;
-//     window.scrollTo(0,heightTop);
-//   }
-//   let intervalOff = setInterval(topset, 23);
-// });
-// btnOffer.addEventListener('click', function(evt){
-//   let heightTop = window.pageYOffset;
-//   evt.preventDefault();
-//   heightLink = tarifiId.offsetTop;
-//   function topset() {
-//     if(heightTop > heightLink -50){
-//       clearInterval(intervalOff)
-//     }
-//     heightTop+=40;
-//     window.scrollTo(0,heightTop);
-//   }
-//   let intervalOff = setInterval(topset, 23);
-// });
+tarifiLink.forEach(function(item) {
+item.addEventListener('click',function(evt) {
+ let heightTop = 0;
+  if(item.hash === '#tarifiId'){
+    evt.preventDefault();
+   heightLink = tarifiId.offsetTop;
+} else if (item.hash === '#otzivi'){
+      evt.preventDefault();
+   heightLink = otziviId.offsetTop;
+} else if (item.hash === '#grad'){
+        evt.preventDefault();
+   heightLink = gridId.offsetTop;
+}
+  function topset(){
+    if(heightTop > heightLink -50){
+      clearInterval(intervalOff)
+    }
+    heightTop+=50;
+    window.scrollTo(0,heightTop);
+  }
+  let intervalOff = setInterval(topset, 13);
+  })
+});
 
+btnStart.addEventListener('click', function(evt) {
+  let heightTop = window.pageYOffset;
+  evt.preventDefault();
+  heightLink = gridId.offsetTop;
+  function topset(){
+    if(heightTop > heightLink -50){
+      clearInterval(intervalOff)
+    }
+    heightTop+=40;
+    window.scrollTo(0,heightTop);
+  }
+  let intervalOff = setInterval(topset, 23);
+});
+btnOffer.addEventListener('click', function(evt){
+  let heightTop = window.pageYOffset;
+  evt.preventDefault();
+  heightLink = tarifiId.offsetTop;
+  function topset() {
+    if(heightTop > heightLink -50){
+      clearInterval(intervalOff)
+    }
+    heightTop+=40;
+    window.scrollTo(0,heightTop);
+  }
+  let intervalOff = setInterval(topset, 23);
+});
